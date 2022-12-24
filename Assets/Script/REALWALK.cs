cacf62153306f68fcaa9cbc191809cc7b04fcef1
Anish-Dotcom <anishrao210@gmail.com>
2022-12-23 18:57:08 -0800

    STAYS

--- Assets/Script/REALWALK.cs
+++ Assets/Script/REALWALK.cs
@@ -11,7 +11,7 @@ public class REALWALK : MonoBehaviour
 
     // Use this for initialization
     void Start()  {
-
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame

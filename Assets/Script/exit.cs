25ad2bde9c68752cf046809ec78bbd30950ee5b1
ayanmas <460146@bsd48.org>
2022-12-23 19:07:03 -0800

    house door

--- Assets/Script/exit.cs
+++ Assets/Script/exit.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class areaexit : MonoBehaviour
+public class exit : MonoBehaviour
 {
     public string sceneToLoad;
 

3895bcd25082ad2649c23b327fe2a01b761c4b0d
Anish-Dotcom <anishrao210@gmail.com>
2022-12-23 22:15:48 -0800

    ok

--- Assets/Script/AreaExit.cs
+++ Assets/Script/AreaExit.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class exit : MonoBehaviour
+public class areaexit : MonoBehaviour
 {
     public string sceneToLoad;
     public string areaTransitionName;

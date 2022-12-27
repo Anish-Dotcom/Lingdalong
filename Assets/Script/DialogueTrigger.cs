1bc0fbb7dc1a7876fe9c8e55f3514a83461fb61c
ayanmas <460146@bsd48.org>
2022-12-27 12:22:42 -0800

    dialogue system worky

---
+++ Assets/Script/DialogueTrigger.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+    public GameObject diaBox;
+    public Text diaText;
+    public string dia;
+    public bool playerInRange;
+
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && playerInRange)
+        {
+            if (diaBox.activeInHierarchy)
+            {
+                diaBox.SetActive(false);
+            }
+            else
+            {
+                diaBox.SetActive(true);
+                FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            playerInRange = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            playerInRange = false;
+            diaBox.SetActive(false);
+    }
+}

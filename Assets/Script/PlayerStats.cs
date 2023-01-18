c11788a4ef66b733532afe96f67bef20309fd80f
Anish-Dotcom <anishrao210@gmail.com>
2023-01-17 17:47:25 -0800

    Commit:)

--- Assets/Script/PlayerStats.cs
+++ Assets/Script/PlayerStats.cs
@@ -4,15 +4,76 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
+
+    public string charName;
+    public int playerLevel = 1;
+    public int currentEXP;
+    public int[] expToNextLevel;
+    public int maxLevel = 100;
+    public int baseEXP = 100;
+
+
+    public int currentHP;
+    public int maxHP = 100;
+    public int strength;
+    public int defence;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        expToNextLevel = new int[maxLevel];
+        expToNextLevel[1] = baseEXP;
+
+        for (int i = 2; i < expToNextLevel.Length; i++)
+        {
+            expToNextLevel[i] = Mathf.FloorToInt(expToNextLevel[i - 1] * 1.05f);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            AddExp(100);
+        }
+    }
+
+    public void AddExp(int expToAdd)
+    {
+        currentEXP += expToAdd;
+
+        if (playerLevel < maxLevel)
+        {
+
+
+            if (currentEXP > expToNextLevel[playerLevel])
+            {
+                currentEXP -= expToNextLevel[playerLevel];
+
+                playerLevel++;
+
+                //determine whether to add to strength or defence based on odd or even
+                if (playerLevel % 2 == 0)
+                {
+                    strength++;
+                }
+                else
+                {
+                    defence++;
+                }
+
+                maxHP = Mathf.FloorToInt(maxHP * 1.02f);
+                currentHP = maxHP;
+
+
+            }
+        }
+
+        if (playerLevel >= maxLevel)
+        {
+            currentEXP = 0;
+        }
     }
 }

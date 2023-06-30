42bfbfc4fd93e0bf8adf7c128d1c0b31d68805b4
Anish-Dotcom <anishrao210@gmail.com>
2023-06-30 13:44:17 -0700

    Naga

---
+++ Assets/CatMovement.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class CatMovement : MonoBehaviour
+{
+    public float speed = 5f; // The speed at which the NPC moves
+    public Vector2 targetPosition; // The target position to move towards
+    public bool a = false;
+
+
+    private Rigidbody2D rb2d;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        // Calculate the direction vector towards the target
+        Vector2 direction = (targetPosition - rb2d.position).normalized;
+
+        // Calculate the velocity by multiplying the direction with the speed
+        Vector2 velocity = direction * speed;
+
+        // Assign the velocity to the Rigidbody2D's velocity property
+        if (a == true)
+        {
+            rb2d.velocity = velocity;
+        }
+    }
+}

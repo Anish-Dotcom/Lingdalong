df21a13198810ddab46673917e4acfda128e5a08
ayanmas <460146@bsd48.org>
2023-07-12 15:28:16 -0700

    added the pick up feature. ill create the inventory later

---
+++ Assets/Script/PlayerScripts/pickup.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pickup : MonoBehaviour
+{
+    public Sprite itemsprite;
+    public string itemname;
+    public Image itemimage;
+    public Text itemtext;
+    public GameObject pickupbox;
+    public GameObject item;
+    public GameObject pickuptext;
+    public bool playerInRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && playerInRange)
+        {
+            if (item.activeInHierarchy)
+            {
+                pickupbox.SetActive(true);
+                itemimage.sprite = itemsprite;
+                itemtext.text = itemname;
+                StartCoroutine(goaway());
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            playerInRange = true;
+            pickuptext.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            playerInRange = false;
+            pickuptext.SetActive(false);
+    }
+
+    IEnumerator goaway()
+    {
+        yield return new WaitForSeconds(2);
+        pickupbox.SetActive(false);
+        item.SetActive(false);
+    }
+}

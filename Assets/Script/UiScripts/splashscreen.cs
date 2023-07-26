5a662c57fe726fd798f39328bc6cba923251062f
ayanmas <460146@bsd48.org>
2023-07-25 23:48:14 -0700

    added splash screen huzzah

---
+++ Assets/Script/UiScripts/splashscreen.cs
@@ -0,0 +1,265 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class splashscreen : MonoBehaviour
+{
+    public float speed = 0.1f;
+    public string sceneToLoad;
+    public GameObject anim1;
+    public GameObject anim2;
+    public GameObject anim3;
+    public GameObject anim4;
+    public GameObject anim5;
+    public GameObject anim6;
+    public GameObject anim7;
+    public GameObject anim8;
+    public GameObject anim9;
+    public GameObject anim10;
+    public GameObject anim11;
+    public GameObject anim12;
+    public GameObject anim13;
+    public GameObject anim14;
+    public GameObject anim15;
+    public GameObject anim16;
+    public GameObject anim17;
+    public GameObject anim18;
+    public GameObject anim19;
+    public GameObject anim20;
+    public GameObject anim21;
+    public GameObject anim22;
+    public GameObject anim23;
+    public GameObject anim24;
+    public GameObject anim25;
+    public GameObject anim26;
+    public GameObject anim27;
+    public GameObject anim28;
+    public GameObject anim29;
+    public GameObject anim30;
+    public GameObject anim31;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim1.SetActive(true);
+        StartCoroutine(anime2());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator anime2()
+    {
+        yield return new WaitForSeconds(speed);
+        anim1.SetActive(false);
+        anim2.SetActive(true);
+        StartCoroutine(anime3());
+    }
+    IEnumerator anime3()
+    {
+        yield return new WaitForSeconds(speed);
+        anim2.SetActive(false);
+        anim3.SetActive(true);
+        StartCoroutine(anime4());
+    }
+    IEnumerator anime4()
+    {
+        yield return new WaitForSeconds(speed);
+        anim3.SetActive(false);
+        anim4.SetActive(true);
+        StartCoroutine(anime5());
+    }
+    IEnumerator anime5()
+    {
+        yield return new WaitForSeconds(speed);
+        anim4.SetActive(false);
+        anim5.SetActive(true);
+        StartCoroutine(anime6());
+    }
+    IEnumerator anime6()
+    {
+        yield return new WaitForSeconds(speed);
+        anim5.SetActive(false);
+        anim6.SetActive(true);
+        StartCoroutine(anime7());
+    }
+    IEnumerator anime7()
+    {
+        yield return new WaitForSeconds(speed);
+        anim6.SetActive(false);
+        anim7.SetActive(true);
+        StartCoroutine(anime8());
+    }
+    IEnumerator anime8()
+    {
+        yield return new WaitForSeconds(speed);
+        anim7.SetActive(false);
+        anim8.SetActive(true);
+        StartCoroutine(anime9());
+    }
+    IEnumerator anime9()
+    {
+        yield return new WaitForSeconds(speed);
+        anim8.SetActive(false);
+        anim9.SetActive(true);
+        StartCoroutine(anime10());
+    }
+    IEnumerator anime10()
+    {
+        yield return new WaitForSeconds(speed);
+        anim9.SetActive(false);
+        anim10.SetActive(true);
+        StartCoroutine(anime11());
+    }
+    IEnumerator anime11()
+    {
+        yield return new WaitForSeconds(speed);
+        anim10.SetActive(false);
+        anim11.SetActive(true);
+        StartCoroutine(anime12());
+    }
+    IEnumerator anime12()
+    {
+        yield return new WaitForSeconds(speed);
+        anim11.SetActive(false);
+        anim12.SetActive(true);
+        StartCoroutine(anime13());
+    }
+    IEnumerator anime13()
+    {
+        yield return new WaitForSeconds(speed);
+        anim12.SetActive(false);
+        anim13.SetActive(true);
+        StartCoroutine(anime14());
+    }
+    IEnumerator anime14()
+    {
+        yield return new WaitForSeconds(speed);
+        anim13.SetActive(false);
+        anim14.SetActive(true);
+        StartCoroutine(anime15());
+    }
+    IEnumerator anime15()
+    {
+        yield return new WaitForSeconds(speed);
+        anim14.SetActive(false);
+        anim15.SetActive(true);
+        StartCoroutine(anime16());
+    }
+    IEnumerator anime16()
+    {
+        yield return new WaitForSeconds(speed);
+        anim15.SetActive(false);
+        anim16.SetActive(true);
+        StartCoroutine(anime17());
+    }
+    IEnumerator anime17()
+    {
+        yield return new WaitForSeconds(speed);
+        anim16.SetActive(false);
+        anim17.SetActive(true);
+        StartCoroutine(anime18());
+    }
+    IEnumerator anime18()
+    {
+        yield return new WaitForSeconds(speed);
+        anim17.SetActive(false);
+        anim18.SetActive(true);
+        StartCoroutine(anime19());
+    }
+    IEnumerator anime19()
+    {
+        yield return new WaitForSeconds(speed);
+        anim18.SetActive(false);
+        anim19.SetActive(true);
+        StartCoroutine(anime20());
+    }
+    IEnumerator anime20()
+    {
+        yield return new WaitForSeconds(speed);
+        anim19.SetActive(false);
+        anim20.SetActive(true);
+        StartCoroutine(anime21());
+    }
+    IEnumerator anime21()
+    {
+        yield return new WaitForSeconds(speed);
+        anim20.SetActive(false);
+        anim21.SetActive(true);
+        StartCoroutine(anime22());
+    }
+    IEnumerator anime22()
+    {
+        yield return new WaitForSeconds(speed);
+        anim21.SetActive(false);
+        anim22.SetActive(true);
+        StartCoroutine(anime23());
+    }
+    IEnumerator anime23()
+    {
+        yield return new WaitForSeconds(speed);
+        anim22.SetActive(false);
+        anim23.SetActive(true);
+        StartCoroutine(anime24());
+    }
+    IEnumerator anime24()
+    {
+        yield return new WaitForSeconds(speed);
+        anim23.SetActive(false);
+        anim24.SetActive(true);
+        StartCoroutine(anime25());
+    }
+    IEnumerator anime25()
+    {
+        yield return new WaitForSeconds(speed);
+        anim24.SetActive(false);
+        anim25.SetActive(true);
+        StartCoroutine(anime26());
+    }
+    IEnumerator anime26()
+    {
+        yield return new WaitForSeconds(speed);
+        anim25.SetActive(false);
+        anim26.SetActive(true);
+        StartCoroutine(anime27());
+    }
+    IEnumerator anime27()
+    {
+        yield return new WaitForSeconds(speed);
+        anim26.SetActive(false);
+        anim27.SetActive(true);
+        StartCoroutine(anime28());
+    }
+    IEnumerator anime28()
+    {
+        yield return new WaitForSeconds(speed);
+        anim27.SetActive(false);
+        anim28.SetActive(true);
+        StartCoroutine(anime29());
+    }
+    IEnumerator anime29()
+    {
+        yield return new WaitForSeconds(speed);
+        anim28.SetActive(false);
+        anim29.SetActive(true);
+        StartCoroutine(anime30());
+    }
+    IEnumerator anime30()
+    {
+        yield return new WaitForSeconds(speed);
+        anim29.SetActive(false);
+        anim30.SetActive(true);
+        StartCoroutine(anime31());
+    }
+    IEnumerator anime31()
+    {
+        yield return new WaitForSeconds(1f);
+        anim30.SetActive(false);
+        anim31.SetActive(true);
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
